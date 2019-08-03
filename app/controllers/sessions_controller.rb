b9238a40c783d6f320155fc600ecfc28249c5fea
Herna Michel <labellehernamichel@yahoo.fr>
2019-08-03 21:15:21 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < ApplicationController
+  
+  def create
+    @user = User.find_by(name: params[:name])
+    if @user.try(:authenticate,params[:password])
+      session[:user_id] = @user.id
+    else
+      return head(:forbidden) 
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    if params[:password] == params[:password_confirmation]
+      user = User.new(user_params)
+      user.save
+      session[:user_id] = user.id
+    else
+      redirect_to 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:password,:password_confirmation)
   end
 end
